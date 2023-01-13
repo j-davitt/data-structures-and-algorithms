87f3d6feedddbcead5e931da51291a9038c1046e
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-13 18:29:59 +0000

    complete 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -34,9 +34,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 ------------------------------------------------------------------------------------------------ */
 
-const addPurchases = (arr) => {
-  // Solution code here...
-};
+const addPurchases = (arr) => arr.reduce((a, b) => a + b.purchasePrice, 0);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
