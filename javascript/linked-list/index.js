4754abc517a63b7cbb5ce0c961ba127f137c11c1
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-17 23:47:26 +0000

    create functions and tests

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,34 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insert(value){
+    const oldHead = this.head;
+    // this.head = new Node(value, this.head); passing current head to next node (one liner)
+    this.head = new Node(value);
+    this.head.next = oldHead;
+  }
+
+  toString() {
+    let current = this.head;
+    let str = '';
+    while (current) {
+      str += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+
+    return str += 'NULL';
+  }
+}
+
+class Node {
+  constructor(value, next=null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 module.exports = LinkedList;
