f0ce7df8ba637d502012ce8e64251dc0b39dab77
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-27 21:47:52 +0000

    complete challenge 11

---
+++ javascript/stacks/README.md
@@ -0,0 +1,28 @@
+# Stacks and Queues
+
+Colab with Jordan, Adrienne, Ty, and Jeremy
+
+## Challenge
+
+Create a new class called pseudo queue.
+Do not use an existing Queue.
+Instead, this PseudoQueue class will implement our standard queue interface (the two methods listed below),
+Internally, utilize 2 Stack instances to create and manage the queue
+Methods:
+enqueue
+Arguments: value
+Inserts a value into the PseudoQueue, using a first-in, first-out approach.
+dequeue
+Arguments: none
+Extracts a value from the PseudoQueue, using a first-in, first-out approach.
+
+## Approach & Efficiency
+
+
+## API
+
+
+## Whiteboard
+
+![Whiteboard](../assets/challenge11.png)
+
--- javascript/stacks/index.js
+++ javascript/stacks/index.js
@@ -32,7 +32,7 @@ class Stack {
   }
 
   isEmpty() {
-    return!this.top;
+    return !this.top;
   }
 }
 
@@ -80,11 +80,36 @@ class Queue {
   }
 
   isEmpty() {
-    return!this.front;
+    return !this.front;
+  }
+
+}
+
+class PseudoQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  enqueue(value) {
+    while (!this.stack1.isEmpty()) {
+      this.stack2.push(this.stack1.pop());
+    }
+    this.stack1.push(value);
+    while (!this.stack2.isEmpty()) {
+      this.stack1.push(this.stack2.pop());
+    }
+  }
+  dequeue() {
+    if (this.stack1.isEmpty()) {
+      return null;
+    } else {
+      return this.stack1.pop();
+    }
   }
 
 }
 
 
-module.exports = { Stack, Queue };
+module.exports = { Stack, Queue, PseudoQueue };
 
