c27b103b27a837b66c746650566b15a2145d3ff2
Joe Davitt <joseph.j.davitt@gmail.com>
2023-03-06 21:37:58 +0000

    add tests

--- javascript/trees/trees.test.js
+++ javascript/trees/trees.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Node, Tree } = require('./index');
+const { Node, Tree, BinaryTree } = require('./index');
 
 describe('Tree', () => {
   it('Should create a tree', () => {
@@ -11,6 +11,26 @@ describe('Tree', () => {
 
     expect(tree.root.left.value).toEqual(1);
     expect(tree.root.right.right).toBeNull();
+  });
+  it('Should return max value', () => {
+    let tree = new Tree();
+    tree.root = new Node(5);
+    tree.root.left = new Node(1);
+    tree.root.right = new Node(8);
+
+    expect(tree.getMax()).toEqual(8);
+  });
+  it('adds to binary search tree', () => {
+    let bTree = new BinaryTree();
+    bTree.add(8);
+    bTree.add(3);
+    bTree.add(5);
+    bTree.add(10);
+
+    expect(bTree.root.value).toEqual(8);
+    expect(bTree.root.left.value).toEqual(3);
+    expect(bTree.root.left.right.value).toEqual(5);
+    expect(bTree.root.right.value).toEqual(10);
   });
 });
 
