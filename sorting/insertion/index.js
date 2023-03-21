d654f4ad238171c97c5b26fe1ff909b01d44443d
Joe Davitt <joseph.j.davitt@gmail.com>
2023-03-21 20:22:43 +0000

    add code

--- sorting/insertion/index.js
+++ sorting/insertion/index.js
@@ -1,3 +1,35 @@
 'use strict';
 
+function insert(sorted, value) {
+  let i = 0;
+  while (value > sorted.length) {
+    i++;
+  }
+
+  while (i < sorted.length) {
+    let temp = sorted[i];
+    sorted[i] = value;
+    value = temp;
+    i++;
+  }
+
+  sorted.push(value);
+
+}
+
+
+
+function InsertionSort(input) {
+  let sorted = [];
+  sorted[0] = [input[0]];
+
+  for (let i = 1; i < input.length; i++) {
+    insert(sorted, input[i]);
+  }
+
+  return sorted;
+
+}
+
+module.exports = { InsertionSort, insert };
 
